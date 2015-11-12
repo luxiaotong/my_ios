02091cee36abd3c05097786e93399455b39136bb
luxiaotong <luxiaotong713@sina.com>
2015-11-12 11:26:33 +0800

    Add bug Add animation for bug

--- PicnicBasket/PicnicBasket/ViewController.m
+++ PicnicBasket/PicnicBasket/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *basketBottom;
 @property (weak, nonatomic) IBOutlet UIImageView *fabricTop;
 @property (weak, nonatomic) IBOutlet UIImageView *fabricBottom;
+@property (weak, nonatomic) IBOutlet UIImageView *bug;
 
 @end
 
@@ -31,29 +32,83 @@
     CGRect fabricBottomFrame = self.fabricBottom.frame;
     fabricBottomFrame.origin.y = self.view.bounds.size.height;
     
-    [UIView animateWithDuration:5.0
-                          delay:2.0
+    [UIView animateWithDuration:2.0
+                          delay:1.0
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
                          self.basketTop.frame = basketTopFrame;
                          self.basketBottom.frame = basketBottomFrame;
                      }
                      completion:^(BOOL finished){
-                         [UIView animateWithDuration:5.0
-                                               delay:2.0
+                         [UIView animateWithDuration:2.0
+                                               delay:1.0
                                              options:UIViewAnimationOptionCurveEaseOut
                                           animations:^{
                                               self.fabricTop.frame = fabricTopFrame;
                                               self.fabricBottom.frame = fabricBottomFrame;
                                           }completion:^(BOOL finished){
-                                              
                                           }];
                      }];
+    
+    //Move for bug
+    [self moveToLeft];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)moveToLeft {
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+                        options:(UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAllowUserInteraction)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         [UIView setAnimationDidStopSelector:@selector(faceToRight)];
+                         self.bug.center = CGPointMake(75, 200);
+                     }
+                     completion:^(BOOL finished){
+                     }];
+}
+
+- (void)faceToRight {
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+                        options:(UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAllowUserInteraction)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         [UIView setAnimationDidStopSelector:@selector(moveToRight)];
+                         self.bug.transform = CGAffineTransformMakeRotation(M_PI);
+                     }
+                     completion:^(BOOL finished){
+                     }];
+}
+
+- (void)moveToRight {
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+                        options:(UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAllowUserInteraction)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         [UIView setAnimationDidStopSelector:@selector(faceToLeft)];
+                         self.bug.center = CGPointMake(230, 250);
+                     }
+                     completion:^(BOOL finished){
+                     }];
+}
+
+- (void)faceToLeft {
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+                        options:(UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAllowUserInteraction)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         [UIView setAnimationDidStopSelector:@selector(moveToLeft)];
+                         self.bug.transform = CGAffineTransformMakeRotation(0);
+                     }
+                     completion:^(BOOL finished){
+                     }];
 }
 
 @end
