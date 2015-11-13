a05aade034315c3c62231df498571705abd6885f
luxiaotong <luxiaotong713@sina.com>
2015-11-13 11:13:41 +0800

    Init

---
+++ UIAnimationSamples/UIAnimationSamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAnimationSamples
+//
+//  Created by 逯晓瞳 on 15/11/13.
+//  Copyright © 2015年 逯晓瞳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAnimationSamples/UIAnimationSamples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIAnimationSamples
+//
+//  Created by 逯晓瞳 on 15/11/13.
+//  Copyright © 2015年 逯晓瞳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
