9d4518bf124abb1590a340a590640f3d4d115fe1
baidu <baidu@MacBook-Pro.local>
2015-12-08 18:37:42 +0800

    Test for life cycle

---
+++ AppLifeCycle/AppLifeCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppLifeCycle
+//
+//  Created by baidu on 15/12/8.
+//  Copyright © 2015年 Luxiaotong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
