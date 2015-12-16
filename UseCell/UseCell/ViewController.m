8b1a16e9b742d26e8d997d289963a17270cc05ff
luxiaotong <luxiaotong@baidu.com>
2015-12-16 19:36:30 +0800

    Add image Add four UITableViewCellStyle

--- UseCell/UseCell/ViewController.m
+++ UseCell/UseCell/ViewController.m
@@ -25,14 +25,49 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-reuseIdentifier:@"UITableViewCell"];
-    cell.textLabel.text = @"123";
+    UITableViewCell *cell = nil;
+    
+    if ( indexPath.row == 0 ) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                       reuseIdentifier:@"UITableViewCell"];
+        cell.imageView.image = [UIImage imageNamed:@"伊斯坦堡.jpeg"];
+        cell.textLabel.text = @"defalut";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+    } else if ( indexPath.row == 1 ) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                                       reuseIdentifier:@"UITableViewCell"];
+        cell.imageView.image = [UIImage imageNamed:@"伊斯坦堡.jpeg"];
+        cell.textLabel.text = @"value1";
+        cell.detailTextLabel.text = @"value1";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    } else if ( indexPath.row == 2 ) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2
+                                                       reuseIdentifier:@"UITableViewCell"];
+//        cell.imageView.image = [UIImage imageNamed:@"伊斯坦堡.jpeg"];
+        cell.textLabel.text = @"value2";
+        cell.detailTextLabel.text = @"value2";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+    } else if ( indexPath.row == 3 ) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                                       reuseIdentifier:@"UITableViewCell"];
+        cell.imageView.image = [UIImage imageNamed:@"伊斯坦堡.jpeg"];
+        cell.textLabel.text = @"subtitle";
+        cell.detailTextLabel.text = @"subtitle";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return 4;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
