194ea3b54a26252208f9b08405c4abf35923a466
luxiaotong <luxiaotong@baidu.com>
2016-03-07 17:45:39 +0800

    Switch two storyboards with program

---
+++ TestForStoryBoard/TestForStoryBoard/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  TestForStoryBoard
+//
+//  Created by baidu on 16/3/2.
+//  Copyright © 2016年 Luxiaotong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
